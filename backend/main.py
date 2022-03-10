5220dff0456bd2c406251f275bdea7b48a96f0d2
santosh <santosh@torcai.com>
2022-03-10 12:37:32 +0530

    connect to DB done-2

--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,21 @@
 from distutils.version import Version
 from fastapi import FastAPI
 from core.config import Settings
+from db.session import engine
+from db.base_class import Base 
 
-app = FastAPI(title=Settings.PROJECT_TITLE, version=Settings.PROJECT_VERSION)
+def create_tables():
+    Base.metadata.create_all(bind=engine)
+
+def start_application():
+    app = FastAPI(title=Settings.PROJECT_TITLE, version=Settings.PROJECT_VERSION)
+    create_tables()
+    return app
+
+app = start_application()
 
 @app.get("/")
 def hello_api():
     return {"detail":"Hello World Yo"}
+
+
