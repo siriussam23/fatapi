8da2972106546747488a8afed40e0aca48762de4
santosh <santosh@torcai.com>
2022-03-22 15:55:55 +0530

    dependency inj_2

--- backend/db/session.py
+++ backend/db/session.py
@@ -1,8 +1,21 @@
+from email.generator import Generator
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 from core.config import Settings
+from typing import Generator
+
+
 SQLALCHEMY_DATABASE_URL = Settings.DATABASE_URL
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit= False, autoflush=False, bind=engine)
+
+
+def get_db() -> Generator:
+    try:
+        db = SessionLocal()
+        yield db
+
+    finally:
+        db.close()
